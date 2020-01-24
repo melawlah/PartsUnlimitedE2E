39ae86ff2092dc4fb94a76949f74a2889ccb23ae
melawlah <lawlahruth@gmail.com>
2020-01-24 12:46:58 +0100

    second comment

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+
+    //second comment
     public class Category
     {
         public int CategoryId { get; set; }
